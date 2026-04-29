272761940df8b501cbed8e0ae7a243773fe9bb95
ankitsinghbash <ankitsingh4734414code@gmail.com>
2026-04-29 15:45:30 +0530

    fix render deploy and cors

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,19 @@ const PORT = process.env.PORT || 5001;
 
 const allowedOrigins = [
   "http://localhost:3000",
+  "https://sikhobusiness.com",
+  "https://seekho-business.vercel.app",
   process.env.FRONTEND_URL
 ].filter(Boolean) as string[];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true
 }));
 
